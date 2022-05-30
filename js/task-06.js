09678270e870af7e5768609cf0a3b62fca54315e
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-05-30 22:16:10 +0300

    hw fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,5 @@
 const getLength = document.querySelector("#validation-input").onblur = function () {
-    if (this.getAttribute('data-length') === this.value.length) {
+    if (this.getAttribute('data-length') >= this.value.length) {
         this.classList.remove('invalid');
         this.classList.add('valid');
     } else {
