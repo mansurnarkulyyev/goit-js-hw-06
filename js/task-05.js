ae1a05d6ee2f7b2ca29ba5c8295e10e41843dcda
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-12-20 17:16:57 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -6,7 +6,6 @@ const refs = {
 refs.input.addEventListener('focus', onInputFocus);
 refs.input.addEventListener('blur', onInputBlur);
 
-// refs.input.addEventListener('change', onInputChange);//для текста в инпуте нельзя использовать change можно только радио кнопок и чекбексах.
 refs.input.addEventListener('input', onInputChange);
 
 
@@ -19,14 +18,9 @@ function onInputFocus(event) {
 
 function onInputBlur() {
     console.log(refs.span.textContent = 'Anonymous');
-    // refs.span.textContent = event.currentTarget.value;
-    // console.log(event);
-
 }
 
 function onInputChange(event) {
-
-    // console.log(event.currentTarget.value);
     refs.span.textContent = event.currentTarget.value;
 
     if (event.currentTarget.value === "") {
@@ -34,30 +28,4 @@ function onInputChange(event) {
     }
 };
 
-
-// 2v
-// const textInput = document.querySelector("#name-input");
-// const output = document.querySelector("#name-output");
-
-// textInput.addEventListener("input", (event) => {
-//     output.textContent = event.currentTarget.value;
-
-// });
-
-
-
-// textInput.addEventListener("blur", () => {
-//     textInput.value = "";
-// });
-// textInput.addEventListener("click", () => {
-//     textInput.focus();
-// });
-
-// output.addEventListener("click", () => {
-//     output.blur();
-// });
-
-// textInput.addEventListener("focus", (event) => {
-//     event.target.value = "";
-// });
 
